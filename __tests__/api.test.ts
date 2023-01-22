892525926ba5e778f55239c207d036503f58d6ad
vian21 <pigir101@uotawa.ca>
2023-01-22 17:09:12 -0500

    added test

---
+++ __tests__/api.test.ts
@@ -0,0 +1,17 @@
+import supertest from "supertest";
+import { describe, expect, test } from "vitest";
+
+import app from "../../src/app";
+
+describe("testing the api", () => {
+    app.ready()
+    test("testing /api", () => {
+        supertest("http://localhost:3000")
+            .get("/api")
+            .expect(200)
+            .expect("Content-Type", /json/)
+            .then((response) => {
+                expect(response.body).toEqual("welcome to the api")
+            })
+    })
+})
